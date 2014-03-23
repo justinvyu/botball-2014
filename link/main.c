fe44d762cb1b9c002ef93ac42b0c3b006a970331
Justin Yu <justin.v.yu@gmail.com>
2014-03-22 20:36:58 -0700

    Update and rename seeding_main.c to main.c

--- link/main.c
+++ link/main.c
@@ -3,6 +3,8 @@
 #include "generic.h"
 #include "drive.h"
 
+//#define MAIN
+#define COLOR_TEST
 /*
 	TODO:
 	1. Add in Servo Positions
@@ -23,6 +25,9 @@
 
 #define L_SENSOR 0 //add light sensor port later
 
+#define COL_GREEN 0 //camera channels green/pink
+#define COL_PINK 1
+
 //  DEFINE THISE 
 
 //#define MOT_TREAD 2
@@ -34,8 +39,22 @@
 *		TOP				
 *		BOTTOM  		
 *						
-*/						
+*/		
 
+void init()
+{
+	//add more stuff if need to do moar
+	camera_open(MED_RES);
+}
+
+int get_object_count_both(int ch1, int ch2){
+	if(get_object_count(ch1) < 1 && get_object_count(ch2) < 1){
+		return 0;
+	}
+	return 1;
+}
+
+#ifdef MAIN
 int main()
 {
 	currstate = INIT;
@@ -79,3 +98,40 @@ int main()
 		ao();
 	}
 }
+#endif
+
+// Created on Fri March 21 2014
+
+//drive, generic functions
+
+#ifdef COLOR_TEST
+int main()
+{
+	init();
+	
+	while(1)
+	{
+		//spin tread until see a tribble
+		while(get_object_count_both(COL_GREEN, COL_PINK) == 0){
+			//mav(MOT_TREAD, 700);
+			//msleep(20);
+			printf("nothing");
+		}
+		if(cam_area(COL_GREEN) > 50) //gets area of blob if present
+		{
+			//if green, push into green basket
+			printf("green");
+		}
+		else if(cam_area(COL_PINK) > 50)
+		{
+			//if pink, push into pink basket
+			printf("pink");
+		}
+		else
+		{
+			camera_update();
+		}
+	}
+}
+#endif
+
