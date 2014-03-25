cb723c1ab47d73a4166cebed37aa40eb394569e1
Justin Yu <justin.v.yu@gmail.com>
2014-03-24 23:11:51 -0700

    Update main.c

--- create/main.c
+++ create/main.c
@@ -16,8 +16,6 @@ degrees left and drives to pick up the blocks.
 
 int main()
 {
-  //initializing program
-  
 	set_servo_position(2,OPEN);
 	enable_servos();
 	create_connect();
@@ -25,26 +23,18 @@ int main()
 	shut_down_in(119);
 	arm_up();
 	
-	//drives create to end of start box
-	
 	create_forward(500,750);
-	
-	//create turns 90 degrees
 	
 	create_drive_direct_right(250,-250);
 	msleep(750);
 	create_stop();
 	
-	//create drives to blocks
-	
 	create_forward(500,1250);
-	
-	//create turns 90 degrees
 	
 	create_drive_direct_left(250,-250);
 	msleep(750);
 	create_stop();
 	
-	create_forward(100,250);    //create drives blocks to pick up position
-	set_servo_position(2,CLOSED);     //create picks up blocks
+	create_forward(100,250);
+	set_servo_position(2,CLOSED);    
 }
