2653ca32cd4e5d6951e7e3b8e7af70ccaa4f395a
Justin Yu <justin.v.yu@gmail.com>
2014-03-28 20:31:27 -0700

    Update main.c

--- link/main.c
+++ link/main.c
@@ -1,11 +1,10 @@
 // Created on Sat March 22 2014
 
-//generic and drive functions
 #include "generic.h"
 #include "drive.h"
 
-//#define MAIN
-#define COLOR_TEST
+#define MAIN
+//#define COLOR_TEST
 /*
 	TODO:
 	1. Add in Servo Positions
@@ -13,21 +12,34 @@
 	3. Update drive.h
 	4. Everyone get a GITHUB
 	5. Measure and code
+	
+	Important Measurements and Positions:
+	Robot length (wheel guard to front)- 35 cm
+	Robot width (left to right)- 18 cm
+	Gate open- 213
+	Gate close- 1760
 */
 
 //state system
-#define INIT 100 //initialize
-#define BOX_DRIVE 200	//drive and get the box, place the box to score
-#define TRIBBLE_ONE 300		//get the first tribble pile, sort and score
-#define CROSS 400	//cross the field
-#define TRIBBLE_TWO 500		//get the second tribble pile
-#define SCORE 600	//come back and score
-#define END 700		//finish
+#define s_INIT 100	//initialize
+#define s_TRIBBLE_ONE 300	//get the first tribble pile, sort and score
+#define s_TRIBBLE_TWO 200 //get the second tribble pile
+#define s_CROSS 400		//cross the field
+#define s_TRIBBLE_THREE 500	//get the third tribble pile
+#define s_TRIBBLE_THREE 800 //get the fourth tribble pile
+#define s_SCORE 600		//come back and score
+#define s_END 700	//finish
 
 #define L_SENSOR 0 //add light sensor port later
 
 #define COL_GREEN 0 //camera channels green/pink
 #define COL_PINK 1
+
+#define S_GATE 0
+#define S_CATCHER 2
+
+#define S_OPEN 213
+#define s_CLOSE 1760
 
 //  DEFINE THISE 
 
@@ -36,63 +48,74 @@
 //#define TOUCH_RIGHT
 
 /* Add In Positions     
-*	SERVO POSITIONS :   
-*		TOP				
-*		BOTTOM  		
+*	S_GATE POSITIONS :   
+*		OPEN		213		
+*		CLOSED  	1760	
 *						
 */		
-
-void init()
-{
-	//add more stuff if need to do moar
-	camera_open(LOW_RES);
-}
 
 #ifdef MAIN
 int main()
 {
-	currstate = INIT;
-	state(INIT)
+	currstate = s_INIT;
+	state(s_INIT)
 	{
+		camera_open(LOW_RES);
+		enable_servos();				// enable servos
 		//light_start(L_SENSOR);		// light start
-		next(BOX_DRIVE);
+		next(s_BOX_DRIVE);
 	}
-	state(BOX_DRIVE)
+	
+	state(s_TRIBBLE_ONE)
 	{
-		forward(40.00);				// forward for 40 cm	
-		left(97, ks/2);				// left 90 degrees (97 because the function undershoots)
-		forward(20.00);				// forward 20 cm
-		//measure to get to box
-		//finish by 3/22/14
-		next(TRIBBLE_ONE);
+		right(47,ks/2);		// left 40 degrees (47 because the function undershoots)
+		set_servo_position(S_GATE,S_OPEN);		// open gate
+		forward(56.00);		// forward 56 cm
+		set_servo_position(s_GATE,S_CLOSE);	// close gate
+		next(s_TRIBBLE_TWO);
 	}
-	state(TRIBBLE_ONE)
+	
+	state(s_TRIBBLE_TWO)
 	{
-		//finish by 3/22/14
-		next(CROSS);
+		left(47,ks/2);		//return to line
+		forward(100.00);	//forward 100 cm
+		set_servo_position(S_GATE,S_OPEN);
+		forward(25.00);		//forward 25 cm
+		set_servo_position(S_GATE,S_CLOSE);
+		next(s_CROSS);
 	}
-	state(CROSS)
+	
+	state(s_CROSS)
+	{
+		forward(60.00);		// forward 60 cm to 
+		right(97,ks/2);		// right 90 degrees
+		forward(95.00);   	// forward 95 cm to cross the little bridge
+		next(s_TRIBBLE_THREE);
+	}
+	
+	state(s_TRIBBLE_TWO)
 	{
 		//finish by 3/28/14
-		next(TRIBBLE_TWO);
+		next(s_SCORE);
 	}
-	state(TRIBBLE_TWO)
+	
+	state(s_SCORE)
 	{
 		//finish by 3/28/14
-		next(SCORE);
+		next(s_END);
 	}
-	state(SCORE)
-	{
-		//finish by 3/28/14
-		next(END);
-	}
-	state(END)
+	
+	state(s_END)
 	{
 		printf("finished");
 		ao();
 	}
 }
 #endif
+
+// Created on Fri March 21 2014
+
+//drive, generic functions
 
 #ifdef COLOR_TEST
 int main()
