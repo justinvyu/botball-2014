0bbb407e202bb150979b671c63d4323606d7e848
Justin Yu <justin.v.yu@gmail.com>
2014-03-30 17:52:39 -0700

    Update main.c

--- link_seeding/main.c
+++ link_seeding/main.c
@@ -4,9 +4,9 @@
 #include "drive.h"
 #include "slowservo.h"
 
-//#define MAIN
+#define MAIN
 //#define COLOR_TEST
-#define S_TEST
+//#define TEST
 
 /*
 *	TODO:
@@ -18,7 +18,7 @@
 *	Important Measurements and Positions:
 *	Robot length (wheel guard to front)- 35 cm
 *	Robot width (left to right)- 18 cm
-*	Gate open- 213
+*	Gate open- 213w
 *	Gate close- 1760
 */
 
@@ -34,19 +34,19 @@
 #define s_END 700								// finish
 
 #define L_SENSOR 0 								// add light sensor port later
-#define TOPHAT1 0
-#define TOPHAT2 1
-#define threshold 512							// threshold for tophat sensor
+#define TOPHAT_LEFT 1
+#define TOPHAT_RIGHT 0
+#define threshold 810							// threshold for tophat sensor
+#define spd 800
 
 #define COL_GREEN 0 							// camera channels green/pink
 #define COL_PINK 1
 
-#define S_GATE 1								// gate servo ports
+#define S_GATE 3								// gate servo ports
 #define S_CATCHER 2
 
 #define S_OPEN 200								// servo open/closed positions
 #define S_CLOSE 1760
-
 #define S_UP 1700								// servo open/closed positions
 #define S_DOWN 100
 
@@ -66,6 +66,56 @@
 *		DOWN		100
 */		
 
+void catch()
+{
+	set_servo_position(S_CATCHER,S_DOWN);
+	msleep(1000);
+	//printf("down");
+	set_servo_position(S_CATCHER,S_UP);
+	msleep(1000);
+	//printf("up");
+}
+
+void f_until_black(int sen)
+{
+	while(analog10(sen)<BLACK_SEN_THRESH)
+	{
+		fd(MOT_RIGHT);
+		fd(MOT_LEFT);
+	}
+}
+
+void l_until_black(int sen)
+{
+	while(analog10(sen)<BLACK_SEN_THRESH)
+	{
+		mav(MOT_RIGHT,spd);
+		msleep(1);
+	}
+}
+
+void r_until_black(int sen)
+{
+	while(analog10(sen)<BLACK_SEN_THRESH)
+	{
+		mav(MOT_LEFT,spd);
+		msleep(1);
+	}
+}
+
+void forward_slow(int distance)
+{
+	if(distance < 0l){
+		distance = -distance;
+	}
+	long newdist;
+	newdist = distance*CMtoBEMF;//conversion ratio
+	mrp(MOT_RIGHT,500,newdist*rdistmult);
+	mrp(MOT_LEFT,500,newdist);
+	bmd(MOT_RIGHT);
+	bmd(MOT_LEFT);
+}
+
 #ifdef MAIN
 int main()
 {
@@ -74,40 +124,48 @@ int main()
 	{
 		camera_open(LOW_RES);
 		enable_servos();						// enable servos
+		set_servo_position(S_CATCHER,S_UP);
+		msleep(500);
+		set_servo_position(S_GATE,S_CLOSE);
+		msleep(500);
 		//light_start(L_SENSOR);				// light start
 		next(s_TRIBBLE_ONE);
 	}
 	
 	state(s_TRIBBLE_ONE)
 	{
-		right(45,ks/2);							// right 40 degrees (47 because the function undershoots)
+		right(50,ks/2);							// right 40 degrees (47 because the function undershoots)
 		msleep(100);
 		set_servo_position(S_GATE,S_OPEN);		// open gate
-		forward(56.00);							// forward 56 cm
+		f_until_black(TOPHAT_RIGHT);			// forward until right sensor sees black
+		printf("see black! time to close the gate\n");
 		set_servo_position(S_GATE,S_CLOSE);		// close gate
-		// add forward until seeing black then white, then turn left until see black
-		
-		printf("tribble 1 achieved");
+		printf("tribble 1 achieved\n");
 		next(s_TRIBBLE_TWO);
 	}
 	
 	state(s_TRIBBLE_TWO)
 	{
-		left(47,ks/2);							// return to line
-		forward(100.00);						// forward 100 cm
+		left(38,ks/2);							// return to line
+		forward_slow(40.00);						// forward 100 cm
+		forward(20.00);
 		set_servo_position(S_GATE,S_OPEN);		// open gate
-		forward(25.00);							// forward 25 cm
+		forward(42.00);							// forward 25 cm
 		set_servo_position(S_GATE,S_CLOSE);		// close gate
-		printf("tribble 2 achieved");
+		printf("tribble 2 achieved\n");
 		next(s_CROSS);
 	}
 	
 	state(s_CROSS)
 	{
-		backward(60.00);						// backward 60 cm to middle of the board 
-		right(97,0);							// right 90 degrees
-		forward(95.00);   						// forward 95 cm to cross the little bridge
-		printf("crossed over to other side");
+		right(180,0);
+		forward(32.00);						// backward 60 cm to middle of the board 
+		left(90,0);							// right 90 degrees
+		//WORKS UP TO HERE
+		//PLEASE TEST AND FIX THE CODE BELOW, USE THE F_UNTIL_BLACK and L_UNTIL_BLACK or R_UNTIL_BLACK FOR CONSTISTENCY
+/*-->*/	forward_slow(55.00);   						// forward 95 cm to cross the little bridge
+		forward(40.00);
+		printf("crossed over to other side\n");
 		next(s_TRIBBLE_THREE);
 	}
 	
@@ -129,7 +187,7 @@ int main()
 		set_servo_position(S_GATE,S_OPEN);		// open gate
 		forward(35.00);							// forward to collect tribbles
 		set_servo_position(S_GATE,S_CLOSE);		// close gate
-		printf("tribble 4 achieved");
+		printf("tribble 4 achieved\n");
 		next(s_CROSSOVER);
 	}
 	
@@ -142,7 +200,7 @@ int main()
 		left(90,0);								// turn left to crossover
 		msleep(10);
 		forward(70.00);							// start going forward to score
-		printf("crossed back our side of the field");
+		printf("crossed back our side of the field\n");
 		next(s_SCORE);
 	}
 	
@@ -157,8 +215,9 @@ int main()
 	
 	state(s_END)
 	{
-		printf("finished");
+		printf("finished\n");
 		ao();
+		disable_servos();
 	}
 }
 #endif
@@ -169,40 +228,36 @@ int main()
 	init();
 	
 	while(1)
-	{
-		//spin tread until see a tribble
-		if(get_object_count(COL_GREEN) < 1 && get_object_count(COL_PINK) < 1)){
-			//mav(MOT_TREAD, 700);
-			//msleep(20);
+{
+//spin tread until see a tribble
+if(get_object_count(COL_GREEN) < 1 && get_object_count(COL_PINK) < 1)){
+//mav(MOT_TREAD, 700);
+//msleep(20);
 			printf("nothing");
-		}
-		else if(cam_area(COL_GREEN) > 50) //gets area of blob if present
-		{
-			//if green, push into green basket
+}
+else if(cam_area(COL_GREEN) > 50) //gets area of blob if present
+{
+//if green, push into green basket
 			printf("green");
 		}
 		else if(cam_area(COL_PINK) > 50)
-		{
-			//if pink, push into pink basket
+{
+//if pink, push into pink basket
 			printf("pink");
 		}
 		else
 		{
 			camera_update();
 			msleep(100);
-		}
-	}
+}
+}
 }
 #endif
 
-#ifdef S_TEST
+#ifdef TEST
 int main()
 {
-	enable_servo(S_CATCHER);
-	set_servo_position(S_CATCHER,S_DOWN);
-	msleep(1000);
-	set_servo_position(S_CATCHER,S_UP);
-	msleep(1000);
-	disable_servo(S_CATCHER); 
+	forward(80);
+	msleep(100);
 }
 #endif
