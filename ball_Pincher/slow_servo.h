415039c6483f339991e4f4f9d074b87f6986c35e
Arrowhead81818 <Arrowhead81818@users.noreply.github.com>
2014-04-10 20:40:11 -0700

    Create slow_servo.h

---
+++ ball_Pincher/slow_servo.h
@@ -0,0 +1,19 @@
+/*#define StartPosition  2047
+#define EndPosition    310
+#define RoboStep       30
+#define RoboTimer      400
+*/
+void slow_servo(int time_int, int s_pos, int step, int endpos)
+{
+	int position;
+	printf ("begin");
+	enable_servos();
+	printf ("hi1");
+	
+	for (position = StartPosition; position <= EndPosition; position -= RoboStep)
+	{
+		set_servo_position(2,position);
+		msleep (RoboTimer);
+		printf ("hi%d", position);
+	}
+}
